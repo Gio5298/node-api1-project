fcb08d3436f40753d04edaac704f91b4e1289e1b
Regino P <regino5298@gmail.com>
2020-01-13 15:17:38 -0700

    added create request, and working on viewing

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+const express = require('express');
+
+const User = require('./data/db');
+
+const server = express();
+
+server.get('/', function (request, response) {
+  response.send({ hello: 'Project then' });
+});
+
+server.post('/api/users', (req, res) => {
+  const userData = req.body;
+  User.add(userData)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        errorMessage: 'sorry, we ran into an error creating the user!',
+      });
+    });
+});
+
+server.get('/api/users', (req, res) => {
+  User.find()
+  .then(user => {
+    console.log('User', user);
+    res.status
+  })
+})
+
+const port = 8000;
+server.listen(port, () => console.log(`\n ** api on port: ${port} ** \n`));
