f6421ffe4392e6192fc996932cde78004407e7b8
Regino P <regino5298@gmail.com>
2020-01-13 16:29:03 -0700

    added functionality to view list, specific user, delete and update

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ const User = require('./data/db');
 
 const server = express();
 
+server.use(express.json())
+
 server.get('/', function (request, response) {
   response.send({ hello: 'Project then' });
 });
 
 server.post('/api/users', (req, res) => {
   const userData = req.body;
-  User.add(userData)
+  if (!userData.name || !userData.bio) {
+    res.status(400).json({
+      errorMessage: `Please provide name and bio for user.`
+    })
+  } else {
+    User.insert(userData)
     .then(user => {
       res.status(201).json(user);
     })
@@ -19,16 +26,97 @@ server.post('/api/users', (req, res) => {
       console.log(error);
       res.status(500).json({
         errorMessage: 'sorry, we ran into an error creating the user!',
-      });
-    });
+      })
+    })
+  }
 });
 
+// To see the list of users
 server.get('/api/users', (req, res) => {
   User.find()
   .then(user => {
     console.log('User', user);
-    res.status
+    res.status(200).json(user);
   })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      errorMessage: 'Sorry, we ran into an error getting the list of users.',
+    });
+  });
+});
+
+// To see specific user
+server.get('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+  User.findById(id)
+  .then(user => {
+    if (!user){
+      res.status(404).json({
+        errorMessage: 'The user with the specified id does not exist.'
+      })
+    } else {
+      res.status(200).json(user)
+    }
+  })
+  .catch(errors => {
+    console.log(error);
+    res.status(500).json({
+      errorMessage: 'The user information could not be retrieved.'
+    })
+  })
+})
+
+// To delete a user
+server.delete('/api/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  User.remove(id)
+  .then(deleted => {
+    if (deleted){
+    res.status(200).json({message: 'User succesfully removed', deleted});
+    } else {
+      res.status(404).json({
+        errorMessage: 'The user with that specified id does not exist'
+      })
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({
+      errorMessage: 'sorry'
+    })
+  })
+})
+
+// To update a user
+server.put('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  const body = req.body
+  User.findById(id)
+  .then(user => {
+    if (!user){
+      res.status(404).json({
+        errorMessage: 'The user with the specified id does not exist.'
+      })
+    } else if (!body.name || !body.bio){
+      res.status(400).json({
+        errorMessage: 'Please provide name and bio for user.'
+      })
+    } else {
+      User.update(id, body)
+      .then(info => {
+        res.status(200).json(info)
+      })
+      .catch(error => {
+        console.log(error)
+        res.status(500).json({
+          errorMessage: 'The user information could not be motified.'
+        })
+      })
+    }
+  })
+
 })
 
 const port = 8000;
